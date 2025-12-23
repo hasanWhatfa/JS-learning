9fb0eeaab8a6e54ef06bea6f580b0d38e6673705
hasanWhatfa <wtfyh05@gmail.com>
2025-12-23 06:03:16 -0500

    delete all and search added

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -4,15 +4,17 @@ const addTaskBtn = document.getElementById('addTaskBtn');
 
 const taskList = document.getElemetById('taskList');
 const clearCompletedBtn = document.getElemetById('clearCompletedBtn');
+const clearTasksBtn = document.getElementById('clearTasksBtn');
+const searchInput = document.getElementById('searchInput');
+
 
 let tasks = [];
 
 
-function addTask(){
+function addTask() {
     const taskText = taskInput.value.trim();
-
-    if(taskText !== ""){
-        tasks.push({task:taskText});
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
         taskInput.value = "";
         displayTasks();
     }
@@ -49,7 +51,39 @@ function clearCompletedTasks(){
     displayTasks();
 }
 
+function deleteAllTasks(){
+    tasks = "";
+    taskList.innerHTML = "";
+    displayTasks();
+}
+
+
+function searchForTask(searchQuery){
+    foundTasks = tasks.filter((task)=> task.text.includes(searchQuery.trim()));
+    if(foundTasks.length == 0){
+        alert('no tasks found.');
+        displayTasks()
+        return
+    }
+    else{
+        taskList.innerHTML = "";
+        foundTasks.forEach((task,index)=>{
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}
+            />
+            <label for="task-${index}">${task.text}</label>
+            `;
+            li.querySelector("input").addEventListener("change",()=>{
+                toggleTask(index)
+            });
+            taskList.appendChild(li);
+        })
+    }
+}
 
 
 addTaskBtn.addEventListener("click",addTask);
 clearCompletedBtn.addEventListener("click",clearCompletedTasks)
+clearTasksBtn.addEventListener("click",deleteAllTasks)
+
