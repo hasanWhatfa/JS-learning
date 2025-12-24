5484811ed5075416fa695d0e3a002e16310a7214
hasanWhatfa <wtfyh05@gmail.com>
2025-12-24 06:28:37 -0500

    done

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,8 +3,29 @@ function performOperation(){
     let num2 = parseInt(document.getElementById('input2').value);
 
     if(!isNaN(num1) && !isNaN(num2)){
-        let result = multiply(num1, num2);
-        displayResult(result);
+        let multiplyResult = multiply(num1, num2);
+        let additionResult = add(num1, num2);
+        let subtracionResult = subtract(num1, num2);
+        let divisionResult = division(num1, num2);
+        displayResult([
+            {
+                operation:'mutiply',
+                result:multiplyResult
+            },
+            {
+                operation:'addition',
+                result:additionResult
+            },
+            {
+                operation:'subtract',
+                result:subtracionResult
+                
+            },
+            {
+                operation:'division',
+                result:divisionResult
+            }
+        ]);
     }
     else{
         displayResult('Please enter valid numbersk')
@@ -18,8 +39,20 @@ function multiply(a,b){
     return a * b;
 }
 
-
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function division(a,b){
+    return a/b;
+}
 function displayResult(result){
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `the result is: ${result}`
+    resultElement.innerHTML = `
+    ${result.map((res,idx)=>{
+        return`the result for -${res?.operation} is: ${res.result} \n <br>`
+    }).join('')}
+    `
 }
